dfe4aa85a6571e7f0c34c1a5889e68568ecd2de5
saraalbadryofficial-web <saraalbadryofficial@gmail.com>
2026-08-20 20:38:45 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,34 @@
+window.SARA_CONFIG = {
+  siteUrl: "https://saraelbadryofficials.com/",
+  ga4Id: "",
+  metaPixel: "",
+
+  supabase: {
+    url: "",
+    anonKey: "",
+    table: "readers"
+  },
+
+  socials: {
+    instagram: "https://instagram.com/saraelbadryofficial",
+    facebook: "https://www.facebook.com/saraelbadryofficial",
+    tiktok: "https://www.tiktok.com/@saraelbadryofficial",
+    goodreads: "",
+    email: ""
+  },
+
+  // الروابط الأساسية موجودة داخل الموقع. يمكن إضافة روابط حملات أو متاجر إضافية هنا بدون تعديل التصميم.
+  buyLinks: {
+    "ibnat-habil": [],
+    "insan-tabia-iphone": [],
+    "nesf-hay": [],
+    "al-mashhad": [],
+    "mahma-sajad": [],
+    "feal-hob": [],
+    "ard-allaza": [],
+    "hosn-alshah": [],
+    "om-fi-mohemma": [],
+    "fakiha-moharrama": [],
+    "qosasat-harir": []
+  }
+};
